2f2792b04621cd4b36670b5acd9526a6d6f5e2ef
bchew01 <54953894+bchew01@users.noreply.github.com>
2019-10-19 21:30:05 -0400

    Add files via upload

---
+++ artObject.js
@@ -0,0 +1,28 @@
+function art(name, price, image) {
+	this.name = name;
+	this.price = price;
+	this.image = image;
+}
+
+
+function displayImages() {
+ 	for (var i = 0; i < artArray.length; i++) {
+    	document.write("<li class='smallImage' id='" + i + "'><img src='" + artArray[i].image + "' width='160' height='120'/></li>");
+	}
+}
+
+function makeInfoList() {
+	for (var i = 0; i < artArray.length; i++) {
+		document.write("<li class='info' id='0" + i + "'>" + artArray[i].name + ", " + artArray[i].price + "</li>");
+	}
+}
+
+
+var art0 = new art("Mona Lisa", "$500M", "small_Mona_Lisa.jpg");
+var art1 = new art("Starry Night", "$350M", "starry.jpg");
+var art2 = new art("American Gothic", "$1B", "gothic.jpg");
+var art3 = new art("The Last Supper", "$375M", "supper.jpg");
+var art4 = new art("Guernica", "$425M", "guernica.jpg");
+
+var artArray = [art0, art1, art2, art3, art4];
+
